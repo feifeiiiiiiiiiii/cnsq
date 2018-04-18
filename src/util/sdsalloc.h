0dcfca2927a1c209e799b41b840eed96f0ef5c50
qinpengfei <qinpengfei@jd.com>
2018-04-18 15:18:05 +0800

    util: 增加dict

--- src/util/sdsalloc.h
+++ src/util/sdsalloc.h
@@ -37,7 +37,26 @@
  * the include of your alternate allocator if needed (not needed in order
  * to use the default libc allocator). */
 #include <stdlib.h>
+#ifdef HAVE_MALLOC_SIZE
+#define PREFIX_SIZE (0)
+#else
+#if defined(__sun) || defined(__sparc) || defined(__sparc__)
+#define PREFIX_SIZE (sizeof(long long))
+#else
+#define PREFIX_SIZE (sizeof(size_t))
+#endif
+#endif
+
+#define update_zmalloc_stat_alloc(__n) do { \
+    size_t _n = (__n); \
+    if (_n&(sizeof(long)-1)) _n += sizeof(long)-(_n&(sizeof(long)-1)); \
+    atomicIncr(used_memory,__n); \
+} while(0)
+
+static size_t used_memory = 0;
 
 #define s_malloc malloc
 #define s_realloc realloc
 #define s_free free
+#define zmalloc malloc
+#define zfree free
